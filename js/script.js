5d987c4bb8d969bc03a8d562aa65694ed4e65fb9
Vinicius Vieira <vinicius.dev19@gmail.com>
2022-12-15 11:18:15 -0300

    modulos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initAccordion from "../js/modules/accordion.js";
+import initAnimacaoScroll from "../js/modules/scroll-animacao.js";
+import initScrollSuave from "../js/modules/scroll-suave.js";
+import initTabNav from "../js/modules/tab-nav.js";
+
+initAccordion();
+initAnimacaoScroll();
+initScrollSuave();
+initTabNav();
