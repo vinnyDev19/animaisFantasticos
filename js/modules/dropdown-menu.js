41f4148210d72d19d82d12e1164da6aa63cee2ae
Vinicius Vieira <vinicius.dev19@gmail.com>
2022-12-17 09:02:15 -0300

    Add dropDownMenu

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,17 @@
+import outsideClick from "../modules/outside-click.js";
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+  dropdownMenus.forEach((menu) => {
+    ["click", "touchstart"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add("active");
+    outsideClick(this, ["click", "touchstart"], () => {
+      this.classList.remove("active");
+    });
+  }
+}
+
--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ import initScrollSuave from "../js/modules/scroll-suave.js";
 import initTabNav from "../js/modules/tab-nav.js";
 import initModal from '../js/modules/modal.js';
 import initToolTip from '../js/modules/toolTip.js';
+import initDropdownMenu from '../js/modules/dropdown-menu.js'
+
 initAccordion();
 initAnimacaoScroll();
 initScrollSuave();
 initTabNav();
 initModal();
 initToolTip();
+initDropdownMenu();
