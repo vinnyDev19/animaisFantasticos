b652d65914b23fb6f3d5d0a0a65916b9e7dae5b2
Vinicius Vieira <vinicius.dev19@gmail.com>
2022-12-19 19:46:22 -0300

    add funcionamento

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,19 @@
+export default function initFuncionamento() {}
+
+const funcionamento = document.querySelector("[data-semana]");
+const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+console.log(diasSemana);
+const horasAberto = funcionamento.dataset.horario.split(",").map(Number);
+console.log(horasAberto);
+
+const dataAgora = new Date();
+const diaAgora = dataAgora.getDay();
+const horarioAgora = dataAgora.getHours();
+
+const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+const horarioAberto =
+horarioAgora >= horasAberto[0] && horarioAgora < horasAberto[1];
+if(semanaAberto && horarioAberto){
+    funcionamento.classList.add("ativo");
+}
+
--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initToolTip from "../js/modules/toolTip.js";
 import initDropdownMenu from "../js/modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js"
 import initAnimaNumeros from "./modules/anima-numeros.js"
-
+import initFuncionamento from "./modules/funcionamento.js"
 initAccordion();
 initAnimacaoScroll();
 initScrollSuave();
@@ -17,4 +17,7 @@ initToolTip();
 initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
+initFuncionamento();
+
+
 
