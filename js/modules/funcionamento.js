ea578e033865f8c2656d01f72249223e026c694b
Vinicius Vieira <vinicius.dev19@gmail.com>
2022-12-19 19:55:39 -0300

    Atualiza funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,19 +1,18 @@
-export default function initFuncionamento() {}
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  console.log(diasSemana);
+  const horasAberto = funcionamento.dataset.horario.split(",").map(Number);
+  console.log(horasAberto);
 
-const funcionamento = document.querySelector("[data-semana]");
-const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
-console.log(diasSemana);
-const horasAberto = funcionamento.dataset.horario.split(",").map(Number);
-console.log(horasAberto);
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
 
-const dataAgora = new Date();
-const diaAgora = dataAgora.getDay();
-const horarioAgora = dataAgora.getHours();
-
-const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-const horarioAberto =
-horarioAgora >= horasAberto[0] && horarioAgora < horasAberto[1];
-if(semanaAberto && horarioAberto){
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto =
+    horarioAgora >= horasAberto[0] && horarioAgora < horasAberto[1];
+  if (semanaAberto && horarioAberto) {
     funcionamento.classList.add("ativo");
+  }
 }
-
